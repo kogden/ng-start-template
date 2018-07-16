64bc444a3e0a69c300c1d434b93a7360e681954a
Kevin Ogden <kogden2@albany.edu>
2018-07-16 13:45:55 -0400

    template file, don't push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  // Change with project title
+  title = 'ng-template';
 }
